c3607d2c3daa12f6a22cd24714a93b5a4187b3d2
gaohan <gaohan9595@163.com>
2019-11-27 19:14:29 +0800

    提交的测试

---
+++ main.py
@@ -0,0 +1,66 @@
+import torch
+from torchvision import datasets
+from torchvision import transforms
+from torch.utils.data import DataLoader
+# from lenet5 import Lenet5
+from resnet import ResNet18
+from torch import nn,optim
+
+
+def main():
+    batchsz = 32
+
+    cifar_train = datasets.CIFAR100('cifar', True, transform=transforms.Compose([
+        transforms.Resize((32, 32)),
+        transforms.ToTensor(),
+    ]), download=True)
+    cifar_train = DataLoader(cifar_train, batch_size=batchsz, shuffle=True)
+
+    cifar_test = datasets.CIFAR100('cifar', False, transform=transforms.Compose([
+        transforms.Resize((32, 32)),
+        transforms.ToTensor(),
+    ]), download=True)
+    cifar_test = DataLoader(cifar_test, batch_size=batchsz, shuffle=True)
+
+    x, label = iter(cifar_train).next()
+    print('x:', x.shape, 'label:', label.shape)
+
+    device = torch.device('cuda')
+    # model = Lenet5().to(device)
+    model = ResNet18().to(device)
+    criteon = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=1e-4)
+    print(model)
+
+    for epoch in range(1000):
+        model.train()
+        for batchidx, (x, label) in enumerate(cifar_train):
+            x, label = x.to(device), label.to(device)
+
+            logits = model(x)
+            loss = criteon(logits, label)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print('epoch:', epoch, 'loss:', loss.item())  # 这个loss只是最后一个batch的loss而不是整体的loss
+
+        model.eval()
+        with torch.no_grad():
+            total_correct = 0
+            total_num = 0
+            for x, label in cifar_test:
+                x, label = x.to(device), label.to(device)
+
+                logits = model(x)
+                pred = logits.argmax(dim=1)
+                total_correct += torch.eq(pred, label).float().sum().item()
+                total_num += x.size(0)
+
+            acc = total_correct / total_num
+            print('epoch:', epoch, 'acc:', acc)
+
+
+if __name__ == '__main__':
+    main()
